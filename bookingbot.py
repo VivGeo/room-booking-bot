ad3c783f97f88825512e595626349c7aafdf7157
VivGeo <vivek.george@ryerson.ca>
2017-03-10 02:45:01 -0500

    using bs4 search function, formatted code

--- bookingbot.py
+++ bookingbot.py
@@ -1,16 +1,18 @@
-#Logs into RULA, shows the available rooms
+# Logs into RULA, shows the available rooms
 
 
-#Imports
+# Imports
 import getpass
 from datetime import date
 from selenium import webdriver
 from bs4 import BeautifulSoup
-#Functions
 
-#returns the correct number of days in the month
-def getDays ( num ):
-    months31 = [1,3,5,7,8,10,12]
+
+# Functions
+
+# returns the correct number of days in the month
+def getDays(num):
+    months31 = [1, 3, 5, 7, 8, 10, 12]
     if num in months31:
         print(31)
         return 31
@@ -20,18 +22,15 @@ def getDays ( num ):
     else:
         print(30)
         return 30
-    
-#Main code
-# used to find 8 seats
-def has_eight_seats(tag):
-    return tag.has_attr('data-seats') and tag['data-seats'] == "8"
 
-#User credentials
+
+# Main code
+
+# User credentials
 name = input("username: ")
 password = getpass.getpass("password: ")
 today = date.today()
 day = input("Day of the Month: ")
-
 
 year = today.year
 month = today.month
@@ -40,20 +39,22 @@ month = today.month
 if month < 10:
     month = "0" + str(month)
 
-#Opens an instance of chrome
+# Opens an instance of chrome
 browser = webdriver.Chrome()
-#Goes to room booking webpage
+# Goes to room booking webpage
 browser.get("http://apps.library.ryerson.ca/room_booking/")
-#Login Credentials (Change to input style)
+# Login Credentials (Change to input style)
 userNameElem = browser.find_element_by_id('username')
 userNameElem.send_keys(name)
 passwordElem = browser.find_element_by_id("password")
 passwordElem.send_keys(password)
 linkElem = browser.find_element_by_id('submit')
 linkElem.click()
-#Gets to the desired date (TODO: Change to input)
-browser.get('http://apps.library.ryerson.ca/room_booking/booking/booking_main?month=' + str(year) + str(month) + '&date=' + str(year) + str(month) + str(day))
-#Selects my study group's room booking criteria (5-8 ppl,LCDs, Whiteboard walls)
+# Gets to the desired date (TODO: Change to input)
+browser.get(
+    'http://apps.library.ryerson.ca/room_booking/booking/booking_main?month=' + str(year) + str(month) + '&date=' + str(
+        year) + str(month) + str(day))
+# Selects my study group's room booking criteria (5-8 ppl,LCDs, Whiteboard walls)
 seatElem = browser.find_element_by_xpath("//input[@value='5-8']").find_element_by_xpath("./..")
 seatElem.click()
 LCDElem = browser.find_element_by_xpath("//input[@value='3']").find_element_by_xpath("./..")
@@ -61,9 +62,12 @@ LCDElem.click()
 whiteboardElem = browser.find_element_by_xpath("//input[@value='6']").find_element_by_xpath("./..")
 whiteboardElem.click()
 
-#Locate 8 ppl rooms
+# Locate 8 ppl rooms
 html = browser.page_source
 soup = BeautifulSoup(html,"html.parser")
 
-room_rows = soup.find_all(has_eight_seats)
-print(len(room_rows))
+# room_rows = soup.find_all(has_eight_seats)
+els = soup.find_all(attrs={"data-seats": "8"})
+
+el = els[0]
+
